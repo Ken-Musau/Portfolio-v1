e3f5f181c111691f00bf446c069423a330aa8ed2
Kennedy Musau <kennedy.musauu@gmail.com>
2024-01-01 00:00:33 +0300

    update name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div>Hello World Welcome to 2024</div>;
+  return (
+    <div>
+      Hello World Welcome to 2024
+      <h1>Kennedy Musau Mutunga</h1>
+    </div>
+  );
 };
 
 export default App;
