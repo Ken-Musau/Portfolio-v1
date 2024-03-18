731df016d441a5080a918c732f616b7887a7e1b4
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-18 22:36:50 +0300

    add link

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,14 @@
 import "./sidebar.scss";
+import Links from "./links/Links";
+import ToggleButton from "./toggleButton/ToggleButton";
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="bg">Links</div>
+      <div className="bg">
+        <Links />
+      </div>
+      <ToggleButton />
     </div>
   );
 }
---
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -0,0 +1,5 @@
+function ToggleButton() {
+  return <div>ToggleButton</div>;
+}
+
+export default ToggleButton;
