aca28fba4ce7d901f79f1f9ce85e5aaa20b17138
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-23 18:26:40 +0300

    add hero description

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,6 +3,17 @@ import "./hero.scss";
 function Hero() {
   return (
     <div className="hero">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>Kennedy Musau</h2>
+          <h1>Software Developer</h1>
+          <div className="buttons">
+            <button>See latest works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
+        </div>
+      </div>
       <div className="imageContainer">
         <img src="/passport.png" alt="" />
       </div>
