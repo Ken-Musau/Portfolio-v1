9090ee356c3bc39a98070b12e025e6b873479593
Kennedy Musau <kennedy.musauu@gmail.com>
2024-03-30 00:22:43 +0300

    add box to service

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,8 @@
 import "./services.scss";
 import { motion } from "framer-motion";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsDatabaseCheck } from "react-icons/bs";
+import { GrVmMaintenance } from "react-icons/gr";
 
 function Services() {
   return (
@@ -18,43 +21,37 @@ function Services() {
       </motion.div>
       <motion.div className="listContainer">
         <div className="box">
+          <FaLaptopCode className="icon" />
           <h2>FrontEnd Development</h2>
           <p>
-            I specialize in crafting engaging and responsive user interfaces
-            using the latest frontend technologies. With expertise in HTML, CSS,
-            JavaScript, React, Tailwind CSS, and Sass, I create seamless user
-            experiences that captivate audiences across all devices and
-            browsers. From intuitive UI designs to performance optimization and
-            accessibility enhancements, I strive to deliver websites that not
-            only look great but also drive results. Lets collaborate to bring
-            your vision to life!
+            Crafting captivating and responsive user interfaces using HTML, CSS,
+            JavaScript, React, Tailwind CSS, and Sass. From intuitive design to
+            performance optimization, I deliver websites that excel across all
+            devices and browsers. Let's turn your vision into reality!
           </p>
         </div>
 
         <div className="box">
+          <BsDatabaseCheck className="icon" />
           <h2>Backend Development</h2>
           <p>
-            With a focus on backend development, I specialize in building robust
-            and scalable web applications using industry-leading technologies.
-            My toolkit includes frameworks such as Ruby on Rails for rapid
-            development and Python with Flask for lightweight and efficient
-            solutions. Whether its crafting RESTful APIs, designing database
-            schemas, or implementing complex business logic, I bring expertise
-            and dedication to every project. Lets collaborate to turn your ideas
-            into powerful backend solutions!
+            Specializing in building robust web applications using Ruby on Rails
+            and Python with Flask. Crafting RESTful APIs, designing database
+            schemas, or implementing complex business logic—I bring expertise to
+            every project. Let's turn your ideas into powerful backend
+            solutions!
           </p>
         </div>
 
         <div className="box">
+          <GrVmMaintenance className="icon" />
           <h2>Maintenance and Support</h2>
           <p>
-            Ensuring your website remains secure, up-to-date, and performing
-            optimally is crucial for your online presence. I offer comprehensive
-            maintenance and support services to keep your website running
-            smoothly. From regular updates to security patches, performance
-            optimization, and troubleshooting, Ill handle all aspects of
-            maintaining your website so you can focus on your core business.
-            Lets work together to ensure your website stays at its best!
+            Offering comprehensive maintenance and support services to keep your
+            website secure, up-to-date, and performing optimally. From updates
+            to security patches and troubleshooting, I handle all aspects of
+            maintaining your website. Let's collaborate to ensure your website
+            stays at its best!
           </p>
         </div>
       </motion.div>
